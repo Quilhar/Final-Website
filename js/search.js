44212c8ea83f8be4245b2b2ff03c817e225da7c9
Quilhar <davidtit240@gmail.com>
2024-06-03 14:49:13 -0400

    commit

--- js/search.js
+++ js/search.js
@@ -25,4 +25,34 @@ exitBtn.addEventListener('click', () => {
 
 const submitBtn = document.querySelector(".search-button")
 const results = document.querySelector(".result-con")
-const 
+
+var species = document.getElementById("species")
+var sex = document.getElementById("sex")
+var distance = document.getElementById("distance")
+
+var img1 = document.getElementById("image1");
+var img2 = document.getElementById("image2");
+var img3 = document.getElementById("image3");
+var img4 = document.getElementById("image4");
+var img5 = document.getElementById("image5");
+var img6 = document.getElementById("image6");
+var img7 = document.getElementById("image7");
+var img8 = document.getElementById("image8");
+
+
+
+submitBtn.addEventListener('click', () => {
+    
+    if (results.style.display == "none"){
+        results.style.display = "flex"
+        
+    }
+    else{
+        results.style.display = "none"
+    }
+})
+
+
+function changeImage() {
+    img.src = "/img/cats/cat6.jpg"
+}
