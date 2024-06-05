8c29e7a4c96f894cb1d50284440fcd3201e53202
Quilhar <davidtit240@gmail.com>
2024-06-05 09:51:36 -0400

    commit

--- js/search.js
+++ js/search.js
@@ -28,9 +28,9 @@ const results = document.querySelector(".result-con")
 
 
 
-var species = document.getElementById("species")
-var sex = document.getElementById("sex")
-var distance = document.getElementById("distance")
+var speciesDrop = document.getElementById("species")
+var sexDrop = document.getElementById("sex")
+var distanceDrop = document.getElementById("distance")
 
 const nameBank = ["Parker", "River", "August", "Nova", "Rowan", "Riley", "Avery", "Quinn", "Cameron", "Angel", "Carter", "Dylan", "Ellis", "Scout", "Moss", "Jett", "Sage"]
 const petName1 = document.querySelector(".name1")
@@ -42,7 +42,7 @@ const petName6 = document.querySelector(".name6")
 const petName7 = document.querySelector(".name7")
 const petName8 = document.querySelector(".name8")
 
-const sex = ["Male", "Female"]
+const sexBank = ["Male", "Female"]
 const petSex1 = document.querySelector(".sex1")
 const petSex2 = document.querySelector(".sex2")
 const petSex3 = document.querySelector(".sex3")
@@ -51,6 +51,15 @@ const petSex5 = document.querySelector(".sex5")
 const petSex6 = document.querySelector(".sex6")
 const petSex7 = document.querySelector(".sex7")
 const petSex8 = document.querySelector(".sex8")
+
+const petAge1 = document.querySelector(".age1")
+const petAge2 = document.querySelector(".age2")
+const petAge3 = document.querySelector(".age3")
+const petAge4 = document.querySelector(".age4")
+const petAge5 = document.querySelector(".age5")
+const petAge6 = document.querySelector(".age6")
+const petAge7 = document.querySelector(".age7")
+const petAge8 = document.querySelector(".age8")
 
 var img1 = document.getElementById("image1");
 var img2 = document.getElementById("image2");
@@ -71,6 +80,9 @@ submitBtn.addEventListener('click', () => {
         results.style.display = "none"
     }
 
+    randomName()
+    randomAge()
+    randomSex()
 })
 
 
@@ -112,8 +124,30 @@ function randomName () {
 }
 
 function randomSex () {
-    petSex1.textContent = sex[randomInteger(0, 1)]
+    petSex1.textContent = sexBank[randomInteger(0, 1)]
+    petSex2.textContent = sexBank[randomInteger(0, 1)]
+    petSex3.textContent = sexBank[randomInteger(0, 1)]
+    petSex4.textContent = sexBank[randomInteger(0, 1)]
+    petSex5.textContent = sexBank[randomInteger(0, 1)]
+    petSex6.textContent = sexBank[randomInteger(0, 1)]
+    petSex7.textContent = sexBank[randomInteger(0, 1)]
+    petSex8.textContent = sexBank[randomInteger(0, 1)]
+}
+
+function randomAge() {
+    petAge1.textContent = `${randomInteger(1, 17)} years old`
+    petAge2.textContent = `${randomInteger(1, 17)} years old`
+    petAge3.textContent = `${randomInteger(1, 17)} years old`
+    petAge4.textContent = `${randomInteger(1, 17)} years old`
+    petAge5.textContent = `${randomInteger(1, 17)} years old`
+    petAge6.textContent = `${randomInteger(1, 17)} years old`
+    petAge7.textContent = `${randomInteger(1, 17)} years old`
+    petAge8.textContent = `${randomInteger(1, 17)} years old`
 
 }
 
+
+
+randomName()
+randomAge()
 randomSex()
